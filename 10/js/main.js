6a15437f8a7eba62b724bde2c48d5f528d8256f5
Keks <keks@htmlacademy.ru>
2023-07-20 22:38:16 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,5 @@
+import {data} from './data.js';
+import {renderPictures} from './pictures.js';
+import './modal.js';
+
+renderPictures(data);
---
+++ 10/js/pictures.js
@@ -0,0 +1,31 @@
+// Находим контейнер с изображениями всех карточек
+const picturesContainer = document.querySelector('.pictures');
+
+// Находим шаблон по id и выбираем содержимое шаблона по классу .picture
+const cardTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+// Функция наполнения данными шаблона с использованием ({деструктуризации})
+const fillCardTemplate = ({id, url, description, comments, likes}) => {
+  const element = cardTemplate.cloneNode(true); // клонируем шаблон для многократного использования
+
+  element.dataset.id = id; // присваивается идентификатор карточки по порядку
+  element.querySelector('.picture__img').src = url; // записывается ссылка на карточку
+  element.querySelector('.picture__img').alt = description; // записывается описание карточки
+  element.querySelector('.picture__comments').textContent = comments.length; // записывается только количество коментариев
+  element.querySelector('.picture__likes').textContent = likes; // записывается количество лайков
+
+  return element; // возвращается заполненная карточка
+};
+
+// Функция отрисовки карточек
+export const renderPictures = (data) => {
+  const fragment = document.createDocumentFragment(); // добавление "черного ящика"-контейнера для объектов
+
+  // Функция перебора всех карточек массива и заполнения данными
+  data.forEach((cardObj) => {
+    const element = fillCardTemplate(cardObj);
+    fragment.appendChild(element); // каждую карточку складируем в контейнер
+    // window.console.log(picture); // вывод в консоль данных каждой карточки
+  });
+  picturesContainer.appendChild(fragment);
+};
