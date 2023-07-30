a4bec8e7f221f670e023ec672fd91b755108efea
Soolovey <soolove4y@gmail.com>
2023-07-30 22:11:20 +0300

    Правки

--- js/load.js
+++ js/load.js
@@ -1,12 +1,14 @@
 import {getData} from './api.js';
 import {renderPictures} from './pictures.js';
 import {showAlertError} from './alert.js';
+import {initializeFilters} from './photo-filter.js';
 
 let dataPhotos = null;
 
 try {
   dataPhotos = await getData(); // получаем данные
   renderPictures(dataPhotos); // отрисовываем полученные данные
+  initializeFilters();
 } catch {
   showAlertError('Данные не загружены. Попробуйте обновить страницу'); // вывод ошибки
 }
